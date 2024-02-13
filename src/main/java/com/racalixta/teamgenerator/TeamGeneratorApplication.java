ea45ff19d1b2ef481e35d0be2718a517b90f738a
racalixta <ra.calixta@gmail.com>
2024-02-13 15:49:10 -0300

    Initial Commit

---
+++ src/main/java/com/racalixta/teamgenerator/TeamGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.racalixta.teamgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamGeneratorApplication.class, args);
+	}
+
+}
